0a1c41caee4acdc49f60e0607d6563a0624f72a1
Elena <84009999+Elen4538@users.noreply.github.com>
2023-01-03 23:48:26 +0000

    Update BubbleSort.java

--- Sortings/BubbleSort.java
+++ Sortings/BubbleSort.java
@@ -2,7 +2,7 @@ public class BubbleSort {
     static void bubbleSorting(int arr[]) {
         int size = arr.length;
 
-        for(int i = 0; i < size-1; i++){
+        for(int i = 0; i < size-1; i++) {
             for(int j = 0; j < size-1; j++) {
                 if(arr[j]>arr[j+1]) {
                     int temp = arr[j];
