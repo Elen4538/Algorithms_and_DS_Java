9392ddc849b66d8b14a9ad78cd07764627779dad
Elena <84009999+Elen4538@users.noreply.github.com>
2023-01-03 22:32:08 +0000

    Create BubbleSort.java

---
+++ Sortings/BubbleSort.java
@@ -0,0 +1,27 @@
+public class BubbleSort {
+    static void bubbleSorting(int arr[]) {
+        int size = arr.length;
+
+        for(int i = 0; i < size-1; i++){
+            for(int j = 0; j < size-1; j++) {
+                if(arr[j]>arr[j+1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+    }
+    
+    public static void main(String[] args) {
+    
+        int data[] = {56,23,1,8,6,9};
+        
+        //call method using class name
+        BubbleSort.bubbleSorting(data);
+        
+        for(int i=0; i<data.length; i++) {
+            System.out.print(data[i] + " ");
+        }
+    }
+}
